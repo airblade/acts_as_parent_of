4aede109dba49066394a82454544f86c956ce13c
Andy Stewart <boss@airbladesoftware.com>
2009-04-02 10:04:54 +0100

    Allow multiple declarations of acts_as_parent_of.

--- lib/air_blade/acts_as_parent_of.rb
+++ lib/air_blade/acts_as_parent_of.rb
@@ -8,55 +8,45 @@ module AirBlade
     end
 
     module ActMethods
+
       # Each child in the array should be the plural form,
       # i.e. matching its has_many declaration.
       def acts_as_parent_of(*children)
-        unless included_modules.include? InstanceMethods
-          extend ClassMethods
-          include InstanceMethods
+        children.each do |child|
+          self.send :after_update, "save_#{child}"
+          self.send :validates_associated, "#{child}"
+        end
 
-          children.each do |child|
-            self.send :after_update, "save_#{child}"
-            self.send :validates_associated, "#{child}"
-          end
+        children.each do |child|
+          singular_child = ActiveSupport::Inflector.singularize child
+          code = <<-END
+            def new_#{singular_child}_attributes=(#{singular_child}_attributes)
+              #{singular_child}_attributes.each do |attributes|
+                #{child}.build attributes
+              end
+            end
 
-          children.each do |child|
-            singular_child = ActiveSupport::Inflector.singularize child
-            code = <<-END
-              def new_#{singular_child}_attributes=(#{singular_child}_attributes)
-                #{singular_child}_attributes.each do |attributes|
-                  #{child}.build attributes
+            def existing_#{singular_child}_attributes=(#{singular_child}_attributes)
+              #{child}.reject(&:new_record?).each do |#{singular_child}|
+                attributes = #{singular_child}_attributes[#{singular_child}.id.to_s]
+                if attributes
+                  #{singular_child}.attributes = attributes
+                else
+                  #{child}.delete #{singular_child}
                 end
               end
+            end
 
-              def existing_#{singular_child}_attributes=(#{singular_child}_attributes)
-                #{child}.reject(&:new_record?).each do |#{singular_child}|
-                  attributes = #{singular_child}_attributes[#{singular_child}.id.to_s]
-                  if attributes
-                    #{singular_child}.attributes = attributes
-                  else
-                    #{child}.delete #{singular_child}
-                  end
-                end
+            def save_#{child}
+              #{child}.each do |#{singular_child}|
+                #{singular_child}.save(false)
               end
-
-              def save_#{child}
-                #{child}.each do |#{singular_child}|
-                  #{singular_child}.save(false)
-                end
-              end
-            END
-            module_eval code, __FILE__, __LINE__
-          end
-
+            end
+          END
+          module_eval code, __FILE__, __LINE__
         end
       end
-    end
 
-    module ClassMethods
-    end
-
-    module InstanceMethods
     end
 
   end

22ec2a7cefba4ebc2f337ef0a78a8860e03ebe8e
Andy Stewart <boss@airbladesoftware.com>
2009-06-15 17:26:21 +0100

    Handles has_one associations.

--- lib/air_blade/acts_as_parent_of.rb
+++ lib/air_blade/acts_as_parent_of.rb
@@ -9,15 +9,28 @@ module AirBlade
 
     module ActMethods
 
-      # Each child in the array should be the plural form,
-      # i.e. matching its has_many declaration.
+      # +children+ should be an array of symbols matching the
+      # appropriate +has_many+ and +has_one+ associations.
+      #
+      # For example:
+      #
+      #   class Project < ActiveRecord::Base
+      #     has_many :tasks, :dependent => :destroy
+      #     has_one :budget, :dependent => :destroy
+      #
+      #     acts_as_parent_of :tasks, :budget
+      #   end
+      #
       def acts_as_parent_of(*children)
         children.each do |child|
           self.send :after_update, "save_#{child}"
           self.send :validates_associated, "#{child}"
         end
 
-        children.each do |child|
+        has_manys = children & reflect_on_all_associations(:has_many).map(&:name)
+        has_ones = children & reflect_on_all_associations(:has_one).map(&:name)
+
+        has_manys.each do |child|
           singular_child = ActiveSupport::Inflector.singularize child
           code = <<-END
             def new_#{singular_child}_attributes=(#{singular_child}_attributes)        # def new_task_attributes=(task_attributes)
@@ -41,6 +54,21 @@ module AirBlade
               #{child}.each do |#{singular_child}|                                     #   tasks.each do |task|
                 #{singular_child}.save(false)                                          #     task.save(false)
               end                                                                      #   end
+            end                                                                        # end
+          END
+          module_eval code, __FILE__, __LINE__
+        end
+
+        has_ones.each do |child|
+          code = <<-END
+            def new_#{child}_attributes=(#{child}_attributes)                          # def new_budget_attributes=(budget_attributes)
+              build_#{child} #{child}_attributes                                       #   build_budget budget_attributes
+            end                                                                        # end
+
+            # TODO: existing child
+
+            def save_#{child}                                                          # def save_budget
+              #{child}.save(false)                                                     #   budget.save(false)
             end                                                                        # end
           END
           module_eval code, __FILE__, __LINE__

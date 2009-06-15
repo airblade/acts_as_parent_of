3a0269c26a8ff2bfd04d253dafb3ca78d47140f2
Andy Stewart <boss@airbladesoftware.com>
2009-06-15 16:58:04 +0100

    Show example generated code in comments.

--- lib/air_blade/acts_as_parent_of.rb
+++ lib/air_blade/acts_as_parent_of.rb
@@ -20,28 +20,28 @@ module AirBlade
         children.each do |child|
           singular_child = ActiveSupport::Inflector.singularize child
           code = <<-END
-            def new_#{singular_child}_attributes=(#{singular_child}_attributes)
-              #{singular_child}_attributes.each do |attributes|
-                #{child}.build attributes
-              end
-            end
+            def new_#{singular_child}_attributes=(#{singular_child}_attributes)        # def new_task_attributes=(task_attributes)
+              #{singular_child}_attributes.each do |attributes|                        #   task_attributes.each do |attributes|
+                #{child}.build attributes                                              #     tasks.build attributes
+              end                                                                      #   end
+            end                                                                        # end
 
-            def existing_#{singular_child}_attributes=(#{singular_child}_attributes)
-              #{child}.reject(&:new_record?).each do |#{singular_child}|
-                attributes = #{singular_child}_attributes[#{singular_child}.id.to_s]
-                if attributes
-                  #{singular_child}.attributes = attributes
-                else
-                  #{child}.delete #{singular_child}
-                end
-              end
-            end
+            def existing_#{singular_child}_attributes=(#{singular_child}_attributes)   # def existing_task_attributes=(task_attributes)
+              #{child}.reject(&:new_record?).each do |#{singular_child}|               #   tasks.reject(&:new_record?).each do |task|
+                attributes = #{singular_child}_attributes[#{singular_child}.id.to_s]   #     attributes = task_attributes[task.id.to_s]
+                if attributes                                                          #     if attributes
+                  #{singular_child}.attributes = attributes                            #       task.attributes = attributes
+                else                                                                   #     else
+                  #{child}.delete #{singular_child}                                    #       tasks.delete task
+                end                                                                    #     end
+              end                                                                      #   end
+            end                                                                        # end
 
-            def save_#{child}
-              #{child}.each do |#{singular_child}|
-                #{singular_child}.save(false)
-              end
-            end
+            def save_#{child}                                                          # def save_tasks
+              #{child}.each do |#{singular_child}|                                     #   tasks.each do |task|
+                #{singular_child}.save(false)                                          #     task.save(false)
+              end                                                                      #   end
+            end                                                                        # end
           END
           module_eval code, __FILE__, __LINE__
         end
